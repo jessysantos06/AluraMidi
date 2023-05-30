08417a52313be795bb4f60c0c8daba38e78d86dd
jessysantos06 <127054407+jessysantos06@users.noreply.github.com>
2023-05-30 11:37:37 -0300

    Concluido o projeto Alura Midi!

--- main.js
+++ main.js
@@ -1,5 +1,13 @@
 function tocaSom (seletorAudio) {
     const elemento = document.querySelector(seletorAudio);
+
+    if (elemento && elemento.localName === 'audio') {
+            elemento.play();
+    }
+    else {
+        //alert('Elemento não encontrado');
+        console.log('Elemento não encontrado ou seletor inválido');
+    }
 }
 
 const listaDeTeclas = document.querySelectorAll('.tecla');
